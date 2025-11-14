ac69a437cfb9478fded1ba5b67cfd24c38648f30
Enki A. Barra M <enki.barmel97@ciencias.unam.mx>
2025-11-13 18:12:00 -0600

    chore: changed src/io_operations.jl to now write eigspectra as .txt files

--- cli/plot_eigspectra.jl
+++ cli/plot_eigspectra.jl
@@ -10,14 +10,14 @@ function plot(ARGS)
   arg1 = parse(Int64, ARGS[1])
   arg2 = parse(Int64, ARGS[2])
   rgxs = Regex.(ARGS[3:end])
-  temperature_dirs::Vector{String} = IMRF.filter_dir_names_in_dir(IMRF.SIMULATIONS_DIR, rgxs...)
+  temperature_dirs::Vector{String} = IMRF.filter_dir_names_in_dir(IMRF.SIMULATIONS_DIR, rgxs...; ext=".txt")
   
   if isempty(temperature_dirs)
     @error "no dir in $(IMRF.SIMULATIONS_DIR) matched each of the $(length(temperature_dirs)) different searching criteria"
     return
   end
 
-  IMRF.plot_eigen_spectra(arg1, arg2, temperature_dirs...; persist_eigspectra=true)
+  IMRF.plot_eigen_spectra(arg1, arg2, temperature_dirs...; persist_eigspectra=true, ext=".txt")
 end
 
 plot(ARGS)
